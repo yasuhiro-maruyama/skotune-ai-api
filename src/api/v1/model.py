a6b5a28837e14abc9ce4e68b86056b8a8abc3202
Yasuhiro Maruyama <yasumarunavi@gmail.com>
2025-07-21 22:32:13 +0900

    未作成の機能をコメントアウト

--- src/api/v1/model.py
+++ src/api/v1/model.py
@@ -1,12 +1,13 @@
 # 楽曲レコメンド機能 Router
 from fastapi import APIRouter
-from schema.model import a005001 as a005001_schema
-from src.service.model import a005001 as a005001_service
+
+# from schema.model import a005001 as a005001_schema
+# from src.service.model import a005001 as a005001_service
 
 router = APIRouter()
 
 
 # A005001_点数推測モデル生成API
-@router.post("/score/create")
-def a005001(req: a005001_schema):
-    return a005001_service.a005001(req)
+# @router.post("/score/create")
+# def a005001(req: a005001_schema):
+#     return a005001_service.a005001(req)
