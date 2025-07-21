0e4e4b14691b302baeeb4dfb477519f04d5fe4ad
Yasuhiro Maruyama <yasumarunavi@gmail.com>
2025-07-21 22:24:42 +0900

    共通モジュールを使用するように修正

--- src/service/auth/a001001.py
+++ src/service/auth/a001001.py
@@ -2,10 +2,12 @@ from passlib.hash import bcrypt
 from sqlalchemy.exc import SQLAlchemyError
 from lib.api_constants import RESPONSE_CODE
 from lib.message import DB_MSG
-from utils.utils import error_response
+from utils.utils import success_response, error_response
 from db.ddl.m_user import M_User
 from db.session import SessionLocal
 from schema.auth import a001001 as a001001_schema
+
+AUTH_ERROR_MSG = "ユーザー情報が取得できませんでした。"
 
 
 # A001001_ログイン照会API Service
@@ -16,33 +18,18 @@ def a001001(req: a001001_schema) -> dict:
 
             # データなし
             if row is None:
-                return {
-                    "success_flg": False,
-                    "code": RESPONSE_CODE.NOT_FOUND,
-                    "message": "ユーザー情報が取得できませんでした。",
-                    "response_info": None,
-                }
+                return error_response(RESPONSE_CODE.NOT_FOUND, AUTH_ERROR_MSG)
 
             # パスワード不一致
             if not bcrypt.verify(req.password, row.password):
-                return {
-                    "success_flg": False,
-                    "code": RESPONSE_CODE.AUTH_ERROR,
-                    "message": "ユーザー情報が取得できませんでした。",
-                    "response_info": None,
-                }
+                return error_response(RESPONSE_CODE.AUTH_ERROR, AUTH_ERROR_MSG)
 
             # 取得した情報からパスワードを除外してユーザー情報を設定
             keys_to_extract = ["user_id", "user_name", "administrator_flg"]
             user_info = {key: getattr(row, key) for key in keys_to_extract}
 
             # ユーザー情報が取得できた場合、ユーザー情報を返却
-            return {
-                "success_flg": True,
-                "code": RESPONSE_CODE.SUCCESS,
-                "message": None,
-                "response_info": user_info,
-            }
+            return success_response(user_info)
 
     # ファイル取り込みエラー
     except FileNotFoundError as e:
